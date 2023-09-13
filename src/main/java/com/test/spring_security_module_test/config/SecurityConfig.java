fc01dab8793928d24b1ace999384f4532a56ab8d
kiselev oleg <kiselevoleg2016@gmail.com>
2023-09-13 13:58:56 +0300

    autorization has been upgraded by adding a custom form of authorization

--- src/main/java/com/test/spring_security_module_test/config/SecurityConfig.java
+++ src/main/java/com/test/spring_security_module_test/config/SecurityConfig.java
@@ -7,16 +7,82 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * @author Kiselev Oleg
  */
 @SuppressWarnings({"PMD.MultipleStringLiterals", "PMD.AvoidDuplicateLiterals"})
+@EnableWebSecurity
 @Configuration
+//@EnableGlobalMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permision.DEVELOPERS_READ.getPermision())
+                .requestMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permision.DEVELOPERS_WRITE.getPermision())
+                .requestMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permision.DEVELOPERS_WRITE.getPermision())
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/auth/login").permitAll()
+                .loginProcessingUrl("/auth/login")
+                .failureUrl("/auth/login")
+                .usernameParameter("username")
+                .passwordParameter("userpassword")
+                .defaultSuccessUrl("/auth/success")
+            )
+            .logout(form -> form
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .deleteCookies("JSESSIONID")
+                    .logoutSuccessUrl("/auth/login")
+            )
+            .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User//.withDefaultPasswordEncoder()
+            .withUsername("admin")
+            .password(passwordEncoder().encode("admin"))
+            //.roles(Role.ADMIN.name())
+            .authorities(Role.ADMIN.getAuthorities())
+            .build());
+        manager.createUser(User
+            .withUsername("user")
+            .password(passwordEncoder().encode("user"))
+            //.roles(Role.USER.name())
+            .authorities(Role.USER.getAuthorities())
+            .build());
+        manager.createUser(User
+            .withUsername("user1")
+            .password(passwordEncoder().encode("admin"))
+            .build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+/*@Configuration
 //@EnableGlobalMethodSecurity(prePostEnabled =true)
 public class SecurityConfig {
     @Bean
@@ -33,6 +99,9 @@ public class SecurityConfig {
             .anyRequest()
             .authenticated()
             .and()
+            //.formLogin()
+            //.loginPage("auth/login").permitAll()
+            //.defaultSuccessUrl("/auth/seccess");
             .httpBasic(Customizer.withDefaults());
         return http.build();
     }
@@ -60,7 +129,7 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(admin, user, user1);
     }
 
-    /*private PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
-    }*/
-}
+    //private PasswordEncoder passwordEncoder() {
+    //    return new BCryptPasswordEncoder(12);
+    //}
+}*/
---
+++ src/main/java/com/test/spring_security_module_test/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.test.spring_security_module_test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Kiselev Oleg
+ */
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/success")
+    public String getSuccessPage() {
+        return "success";
+    }
+}
